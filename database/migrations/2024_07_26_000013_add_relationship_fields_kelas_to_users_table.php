ca973669fda3354d8d60373b09b9a3e5c1a2ad53
failamir <38488480+failamir@users.noreply.github.com>
2024-07-28 14:16:18 +0700

    Create 2024_07_26_000013_add_relationship_fields_kelas_to_users_table.php

---
+++ database/migrations/2024_07_26_000013_add_relationship_fields_kelas_to_users_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRelationshipFieldsKelasToUsersTable extends Migration
+{
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->unsignedBigInteger('kelas_id')->nullable();
+            $table->foreign('kelas_id', 'kelas_fk_9976644')->references('id')->on('kelas');
+        });
+    }
+}
